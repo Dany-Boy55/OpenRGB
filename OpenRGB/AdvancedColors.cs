092106fb82addd155fd09010022bbc7cf8644bea
Luis Daniel Rivera Ortiz <danielrivera1280@gmail.com>
2018-07-19 20:13:49 -0500

    working on subclases

---
+++ OpenRGB/AdvancedColors.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRGB
+{
+    /// <summary>
+    /// Provides aditional functionality for operating colors, particularly in HSV space
+    /// </summary>
+    public abstract class AdvancedColors
+    {
+        /// <summary>
+        /// Transforms a color by rotating its hue in the HSV space
+        /// </summary>
+        /// <param name="color">Initial color</param>
+        /// <param name="radians">Angle for hue rotation in radians</param>
+        /// <returns></returns>
+        public static Color RotateHue(Color color, double radians)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Transforms a color by rotating its hue in the HSV space
+        /// </summary>
+        /// <param name="color">Initial color</param>
+        /// <param name="degrees">Angle for hue rotation in degrees</param>
+        /// <returns></returns>
+        public static Color RotateHue(Color color, int degrees)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generates a color structure from HSV values
+        /// </summary>
+        /// <param name="Hue"> Hue between 0 and 1</param>
+        /// <param name="Saturation"> Saturation between 0 and 1</param>
+        /// <param name="Value"> Value between 0 and 1</param>
+        /// <returns></returns>
+        public static Color FromHSV(float Hue, float Saturation, float Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generates a color structure from HSV values
+        /// </summary>
+        /// <param name="Hue"> Hue between 0 and 360 degrees</param>
+        /// <param name="Saturation"> Saturation between 0 and 1</param>
+        /// <param name="Value"> Value between 0 and 1</param>
+        /// <returns></returns>
+        static Color FromHSV(int Hue, float Saturation, float Value)
+        {
+            float chroma = Saturation * Value;
+            float huePrime = Hue / 60;
+            float X = chroma * (1 - Math.Abs((huePrime % 2) - 1));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Constraints a number between 0 and 255
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private int ClampTo8Bit(int input)
+        {
+            int output;
+            if (input > 255)
+                output = 255;
+            else if (input < 0)
+                output = 0;
+            else
+                output = input;
+            return output;
+        }
+
+        /// <summary>
+        /// Constraints a number between 0 and 255
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private int ClampTo8Bit(float input)
+        {
+            int output;
+            if (input > 255)
+                output = 255;
+            else if (input < 0)
+                output = 0;
+            else
+                output = (int) input;
+            return output;
+        }
+
+        /// <summary>
+        /// Constraints a number between 0 and 255
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private int ClampTo8Bit(double input)
+        {
+            int output;
+            if (input > 255)
+                output = 255;
+            else if (input < 0)
+                output = 0;
+            else
+                output = (int)input;
+            return output;
+        }
+
+        /// <summary>
+        /// Constraints a number between 0 and 255
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private int ClampTo8Bit(string input)
+        {
+            try
+            {
+                int output = Int32.Parse(input);
+                if (output > 255)
+                    output = 255;
+                else if (output < 0)
+                    output = 0;
+                return output;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            
+        }
+    }
+}
--- OpenRGB/ColorWheel.cs
+++ OpenRGB/ColorWheel.cs
@@ -7,24 +7,64 @@ using System.Drawing.Configuration;
 namespace OpenRGB
 {
     /// <summary>
-    /// Control that creates a Color Wheel using the HVS space and allows color selection
+    /// Control that creates a Color Wheel using the hue-saturation space
     /// </summary>
     class ColorWheel : UserControl
     {
         private int radius;
         private Color _color;
+        private Bitmap wheel;
 
         public int Radius { get => radius; }
 
         public ColorWheel(int rad = 50)
         {
             radius = rad;
+            this.Size = new Size(rad * 2, rad * 2);
         }
 
-        private void drawWheel()
+        /// <summary>
+        /// Calculates the distance between a point and the center of the control
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        private int DistanceFromCenter(Point point)
         {
             Point center = new Point(this.Width / 2, this.Height / 2);
-            Graphics g = this.CreateGraphics();
+            double xDist, yDist, totalDist;
+            // Simple pythagorean theorem distance = sqrt(x^2 + y^2)
+            xDist = Math.Abs(point.X - center.X);
+            yDist = Math.Abs(point.Y - center.Y);
+            totalDist = yDist * yDist + xDist*xDist;
+            totalDist = Math.Sqrt(totalDist);
+            return (int)totalDist;
+        }
+
+        /// <summary>
+        /// Generates a bitmap of a colorwheel with the control's specified radius
+        /// </summary>
+        private void generateWheel()
+        {
+            Graphics g = Graphics.FromImage(wheel);
+            for (int x = 0; x < this.Width; x++)
+            {
+                for (int y = 0; y < this.Height; y++)
+                {
+                    Point current = new Point(x, y);
+                    int dist = DistanceFromCenter(current);
+                    if(dist > radius)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        double angle = Math.Atan(y/x);
+
+                    }
+                }
+            }
+
+            throw new NotImplementedException();
         }
     }
 }
